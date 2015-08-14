85e5e1d7ed7404dd6952f9dfdbe523852906ac24
Lisa Folliard <lisaf37@gmail.com>
2015-08-14 09:34:28 -0700

    Write Definition class method, pass spec to unit test returning definition of a word

---
+++ spec/definition_spec.rb
@@ -0,0 +1,13 @@
+require('rspec')
+require('definition')
+
+  describe('Definition') do
+
+    describe('#definition') do
+      it('returns the definition of a word') do
+        new_definition = Definition.new("a trip or journey in a car")
+        # new_definition.save()
+        expect(new_definition.definition()).to(eq("a trip or journey in a car"))
+      end
+    end
+  end
