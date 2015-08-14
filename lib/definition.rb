4c8bb6f54c97357073410d23a20821a48d208272
Lisa Folliard <lisaf37@gmail.com>
2015-08-14 09:30:43 -0700

    Add Gemfile

---
+++ lib/definition.rb
@@ -0,0 +1,10 @@
+class Definition
+
+  define_method(:initialize) do |definition|
+    @definition = definition
+  end
+
+  define_method(:definition) do
+    @definition
+  end
+end
---
+++ lib/word.rb
@@ -0,0 +1,4 @@
+class Word
+
+
+end
