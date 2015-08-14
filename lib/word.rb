998e2a177966e43a336e7600f1841690f8516f95
Lisa Folliard <lisaf37@gmail.com>
2015-08-14 10:29:16 -0700

    Add id, clear, find methods to Class Method Word and pass specs to test proper flow of all

--- lib/word.rb
+++ lib/word.rb
@@ -3,6 +3,7 @@ class Word
 
   define_method(:initialize) do |word|
     @word = word
+    @id = @@words.length().+(1)
   end
 
   define_method(:word) do
@@ -10,11 +11,28 @@ class Word
   end
 
   define_method(:save) do
-    @@words.push(self)    
+    @@words.push(self)
+  end
+
+  define_method(:id) do
+    @id
   end
 
   define_singleton_method(:all) do
     @@words
   end
 
+  define_singleton_method(:clear) do
+    @@words = []
+  end
+
+  define_singleton_method(:find) do |id|
+    new_word = nil
+    @@words.each() do |word|
+      if word.id().eql?(id)
+        new_word = word
+      end
+    end
+    new_word
+  end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -2,11 +2,14 @@ require('rspec')
 require('word')
 
 describe('Word') do
+  before() do
+    Word.clear()
+  end
 
   describe('#word') do
     it('lets you add a new word') do
       new_word = Word.new("cruise")
-      # new_word.save()
+      new_word.save()
       expect(new_word.word()).to(eq("cruise"))
     end
   end
@@ -24,4 +27,30 @@ describe('Word') do
       expect(Word.all()).to(eq([new_word]))
     end
   end
+
+  describe(".clear") do
+    it('empties out all of the saved words') do
+      Word.new("float").save()
+      Word.clear()
+      expect(Word.all()).to(eq([]))
+    end
+  end
+
+  describe("#id") do
+    it('returns the id of the word') do
+      new_word = Word.new("clue")
+      expect(new_word.id()).to(eq(1))
+    end
+  end
+
+  describe(".find") do
+    it("returns a word by its id number") do
+      new_word = Word.new("shoe")
+      new_word.save()
+      new_word2 = Word.new("cat")
+      new_word2.save()
+      expect(Word.find(new_word.id())).to(eq(new_word))
+    end
+  end
+
 end
