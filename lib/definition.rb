3bfeb14b2ebea61991145f93a6a73551d088c09d
Mike Goren <mgoren@mortalwombat.net>
2015-08-16 13:59:30 -0700

    code review feedback

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,39 +1,42 @@
 class Definition
-  @@definitions = []
+  # @@definitions = []
 
-  define_method(:initialize) do |definitions|
-    @definitions = definitions
-    @id = @@definitions.length().+(1)
+  define_method(:initialize) do |definition|
+    @definition = definition
+    # @id = @@definitions.length().+(1)
   end
 
-  define_method(:definitions) do
-    @definitions
+  define_method(:definition) do
+    @definition
   end
 
-  define_method(:id) do
-    @id
-  end
+  # define_method(:id) do
+  #   @id
+  # end
 
-  define_method(:save) do
-    @@definitions.push(self)
-  end
+  # define_method(:save) do
+  #   @@definitions.push(self)
+  # end
 
-  define_singleton_method(:all) do
-    @@definitions
-  end
+  # define_singleton_method(:all) do
+  #   @@definitions
+  # end
 
-  define_singleton_method(:clear) do
-    @@definitions = []
-  end
+  # define_singleton_method(:clear) do
+  #   @@definitions = []
+  # end
 
-  define_singleton_method(:find) do |identification|
-    new_definition = nil
-    @@definitions.each() do |definition|
-      if definition.id().eql?(identification.to_i())
-        new_definition = definition
-      end
-    end
-    new_definition
-  end
+  # define_singleton_method(:find) do |identification|
+  #   new_definition = nil
+  #   @@definitions.each() do |definition|
+  #     if definition.id().eql?(identification.to_i())
+  #       new_definition = definition
+  #     end
+  #   end
+  #   new_definition
+  # end
 
 end
+
+# commented out variables and methods not needed for this exercise
+# definition object just needs to be stored in a word; does not need to be saved into its own array, etc.
